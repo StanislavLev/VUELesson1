a8cc21f9754e2a3fdb99ca02064e632facb719bd
Stas <pschnk@gmail.com>
2018-08-16 14:51:34 -0700

    upgrade to puppy slideshow

--- js/app.js
+++ js/app.js
@@ -1,23 +1,22 @@
 Vue.component('puppy', {
   props: ["puppy_name", "index"],
-  computed: { 
-    puppy_url(){
-      return 'http://www.placepuppy.net/' + (this.index+1) + 'p/400/250';
+  methods: {
+    changeImg(){
+      vm.puppy_url = "http://www.placepuppy.net/" + (this.index+1) + "p/760/475";
+      vm.current_puppy = this.puppy_name;
     }
   },
   template: `
-<div class="puppy-name">
-        <h2>Hello, I'm {{puppy_name}}</h2>
-        <img v-bind:src="puppy_url" alt="puppy picture" />
-        </br>
-</div>
+      <button v-bind:id="'puppy_img' + index" v-on:click="changeImg"><h3>{{puppy_name}}</h3></button>
   `
 })
 
 var vm = new Vue({
   el: '#vue-app',
   data: {
-    greeting: "Hello VUE!",
-	names: ['Lucy', 'Rocky', 'Daisy', 'Buddy', 'Lola', 'Luna', 'Bear', 'Duke', 'Sophie', 'Tucker']
-  }
+    current_puppy: 'Lucy',
+    puppy_url : "http://www.placepuppy.net/1p/760/475",
+    greeting: "Hello VUE! Puppy slideshow!",
+    names: ['Lucy', 'Rocky', 'Daisy', 'Buddy', 'Lola', 'Luna', 'Bear', 'Duke', 'Sophie', 'Tucker']
+  },
 })
